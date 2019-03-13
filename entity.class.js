d9925e8ddcd4ed6d305d13f64b0cb16ece8656c0
Thomas Gainant <tgainant@abmsoft.de>
2019-03-13 13:16:30 +0100

    Basic entity system

--- entity.class.js
+++ entity.class.js
@@ -1,3 +1,22 @@
 class Entity{
-	
+	constructor(id, type){
+		this.id = id;
+		this.type = type;
+
+		this.canvas = null;
+		this.context = null;
+
+		this.x = 0;
+		this.y = 0;
+	}
+
+	init(canvas){
+		this.canvas = canvas;
+		this.context = this.canvas.getContext("2d");
+	}
+
+	render(){
+		/*this.context.fillStyle = "#FF0000";
+		this.context.fillRect(this.x, this.y, 150, 75);*/
+	}
 }
--- render.js
+++ render.js
@@ -12,8 +12,9 @@ else{
 }
 
 var canvas = null;
-
 var entities = [];
+var error = "";
+var loop = null;
 
 function parseRaw(raw){
 	var cleanRaw = raw.replace(/\|.+\|/g, "");
@@ -24,35 +25,75 @@ function parseRaw(raw){
 		parseHead(cleanRaws[1]);
 		parseBody(cleanRaws[2]);
 		parseFooter(cleanRaws[3]);
-		render();
 	}
 	else{
-		document.documentElement.innerHTML = ""+validation;
+		error = validation;
 	}
 }
 
 function validateRaw(cleansRaws){
-	if(cleansRaws.length != 4){
+	if(cleansRaws.length != 5){
 		return "Incorrect number of sections";
 	}
 	else if(cleansRaws[0] != ""){
 		return "Incorrect document start, missing sections";
 	}
 	else if(!cleansRaws[1].startsWith("head")){
-		return "Head section msiplaced";
+		return "Head section misplaced";
 	}
-	else if(!cleansRaws[2].startsWith("body")){
-		return "Body section msiplaced";
+	else if(!cleansRaws[2].startsWith("init")){
+		return "Init section misplaced";
 	}
-	else if(!cleansRaws[3].startsWith("footer")){
-		return "Footer section msiplaced";
+	else if(!cleansRaws[3].startsWith("loop")){
+		return "Loop section misplaced";
+	}
+	else if(!cleansRaws[4].startsWith("async")){
+		return "Footer section misplaced";
 	}
 	
 	return true;
 }
 
 function parseHead(head){
+	let lines = head.split('\n');
+	for(let key in lines){
+		parseHeadLine(lines[key], key);
+	}
+}
 
+function parseHeadLine(line, key){
+	let cleanLine = line.trim();
+	if(cleanLine == "" || cleanLine == "head"){
+	}
+	else{
+		let validation = validateHeadLine(cleanLine, key);
+		if(validation === true){
+			let elements = cleanLine.split("=");
+			let entityId = elements[0].trim();
+			let entityType = elements[1].trim();
+
+			let newEntity = new Entity(entityId, entityType);
+			addEntity(newEntity);
+		}
+		else{
+			error = validation;
+		}
+	}
+}
+
+function validateHeadLine(cleanLine, key){
+	let elements = cleanLine.split("=");
+	if(elements.length != 2){
+		return "Misformed head instruction on line "+key+": <em>"+cleanLine+"</em>";
+	}
+	else if(elements[0].trim() == ""){
+		return "Misformed entity id on line "+key+": <em>"+cleanLine+"</em>";
+	}
+	else if(elements[1].trim() == ""){
+		return "Misformed entity type on line "+key+": <em>"+cleanLine+"</em>";
+	}
+
+	return true;
 }
 
 function parseBody(body){
@@ -65,7 +106,6 @@ function parseFooter(footer){
 
 function render(){
 	var renderHTML = "<html><head></head><body><canvas id=\"lu\"></canvas></body></html>";
-
 	document.documentElement.innerHTML = renderHTML;
 	setTimeout(function(){postRender();}, 1);
 }
@@ -74,6 +114,37 @@ function postRender(){
 	canvas = document.querySelector("#lu");
 	canvas.height = document.body.offsetHeight;
 	canvas.width = document.body.offsetWidth;
+
+	for(let key in entities){
+		entities[key].init(canvas);
+	}
+
+	loop = setInterval(function(){
+		canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+		for(let key in entities){
+			entities[key].render();
+		}
+	}, 1);
 }
 
 parseRaw(raw);
+if(error == null || error == ""){
+	render();
+}
+else{
+	document.documentElement.innerHTML = ""+error;
+}
+
+function addEntity(newEntity){
+	entities.push(newEntity);
+}
+
+function getEntityById(id){
+	let result = null;
+	for(let key in entities){
+		if(entities[key].id == id){
+			result = entities[key];
+		}
+	}
+	return result;
+}
